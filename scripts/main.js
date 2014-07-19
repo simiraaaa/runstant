030994ba068008e2f13d9f56a4a6325c563a320c
phi <phi1618jp@gmail.com>
2014-07-20 00:52:21 +0900

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 
+var data = {};
 var editor = null;
 
 window.onload = function() {
@@ -10,7 +11,6 @@ window.onload = function() {
 var setup = function() {
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/html");
 
     var command = {
         name: "run",
@@ -39,18 +39,52 @@ var setup = function() {
     };
     editor.commands.addCommand(command);
 
+    // 編集の度
+    editor.getSession().on('change', function(e) {
+    	var current = data.current;
+    	data[current] = editor.getValue();
+    	// console.log(e.type);
+    });
+
 
     // デフォルト
     var html = null;
     if (location.hash) {
-    	html = window.atob(location.hash.substr(1));
+    	var dataString = window.atob(location.hash.substr(1));
+    	data = JSON.parse(dataString);
+    	html = data.html;
     }
     else {
-	    html = document.querySelector("#template").innerHTML;
-	    html = html.replace(/__script__/g, 'script');
+    	data = {
+			version: '0.0.1',
+			current: 'js',
+    		html: document.querySelector("#template").innerHTML.replace(/__script__/g, 'script'),
+    		css: document.querySelector("#template-css").innerHTML,
+    		js: document.querySelector("#template-js").innerHTML,
+    	};
     }
 
-    editor.setValue(html);
+    var txt = data[data.current];
+    editor.setValue(txt);
+
+    editor.getSession().setMode("ace/mode/" + getType(data.current));
+
+    // ボタンの設定
+    var buttons = document.querySelectorAll(".code-button");
+    var each = Array.prototype.forEach;
+
+    each.call(buttons, function(button) {
+	    button.onclick = function(e) {
+	    	var key = this.innerHTML;
+	    	
+	    	data.current = key;
+
+	    	editor.setValue(data[key]);
+		    editor.getSession().setMode("ace/mode/" + getType(data.current));
+
+		    return false;
+	    };
+    });
 };
 
 
@@ -62,7 +96,12 @@ var run = function() {
     preview.appendChild(iframe);
 
     var idoc = iframe.contentDocument;
-    var html = editor.getValue();
+    var html = data.html;
+
+    html = html.replace("{script}", data.js);
+    html = html.replace("{style}", data.css);
+
+    console.log(html);
 
     idoc.open();
     idoc.write(html);
@@ -72,8 +111,17 @@ var run = function() {
 
 var save = function() {
     var html = editor.getValue();
+    var dataString = JSON.stringify(data);
 
-	location.hash = window.btoa(html);
+	location.hash = window.btoa(dataString);
 };
 
+
+var getType = function(key) {
+	return {
+		'html': 'html',
+		'css': 'css',
+		'js': 'javascript',
+	}[key];
+};
 
