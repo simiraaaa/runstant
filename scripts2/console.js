2091eb1bcf8e1b4311f3c71e0ccb741b0169bb58
phi <phi1618jp@gmail.com>
2015-05-06 16:49:37 +0900

    fix console

---
+++ scripts2/console.js
@@ -0,0 +1,18 @@
+
+var runstant = runstant || {};
+
+;(function() {
+
+    var Console = function() {
+        this.init();
+    };
+
+    Console.prototype = {
+    	init: function() {
+
+    	},
+    };
+
+    runstant.Console = Console;
+
+})();
